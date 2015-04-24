5bff1189dadee6806cd5269de1f1d7e31ddb1f82
G. Kay Lee <balancetraveller+github@gmail.com>
2015-04-24 13:50:51 +0800

    Build each Foundation component individually

--- gulptasks.js
+++ gulptasks.js
@@ -4,5 +4,7 @@ import GulpLoadPlugins from 'gulp-load-plugins';
 const _ = GulpLoadPlugins();
 
 Gulp.task('build:styles',
-  () => {}
+  () => Gulp.src('source/*.scss')
+            .pipe(_.sass())
+            .pipe(Gulp.dest('build'))
 );
